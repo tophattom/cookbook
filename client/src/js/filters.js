b1f0a7f871a562531255131ff373a1b6edf589df
Jaakko Rinta-Filppula <jaakko.rf@gmail.com>
2014-06-23 09:32:41 +0300

    search now returns recipes that match all of the keywords

--- client/src/js/filters.js
+++ client/src/js/filters.js
@@ -2,17 +2,12 @@ var cbFilters = angular.module('cbFilters', []);
 
 cbFilters.filter('multiFilter', ['filterFilter', function(filterFilter) {
     return function(input, query) {
-        var keywords = query.split(' '),
-            out = [];
+        var keywords = query.split(' ');
 
         keywords.forEach(function(keyword) {
-            out = out.concat(filterFilter(input, keyword));
+            input = filterFilter(input, keyword);
         });
 
-        out = out.filter(function(elem, index) {
-            return out.indexOf(elem) == index;
-        });
-
-        return out;
+        return input;
     };
 }]);
